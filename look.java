986575723ee937ca7ced563b7bb3d05babb40ef8
Elizaweta <liza_pozdn03@mail.ru>
2022-10-22 19:22:22 +0700

    Files look

---
+++ look.java
@@ -0,0 +1,30 @@
+package lab3;
+import java.util.Scanner;
+
+class Look{
+     String color;
+     String eye_color;
+
+     public Look(String color, String eye_color){
+         this.color=color;
+         this.eye_color=eye_color;
+     }
+     public Look(String color){
+         this.color=color;
+     }
+
+     public Look(){
+     }
+
+     public void read(System.in){
+         Scanner in = new Scanner(in);
+         System.out.println("Какой у нее окрас?: ");
+         this.color=in.next();
+         System.out.println("Какой у нее цвет глаз?: ");
+         this.eye_color=in.next();
+     }
+
+     public void display(){
+         System.out.println("Окрас - " + color + ", цвет глаз - " + eye_color);
+     }
+}
